078a17d425f5b73a40d3bc652731eda807302798
Iswarya <iswaryanatrajan@gmail.com>
2025-08-01 17:21:40 +0530

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    // optional for dev
-    historyApiFallback: true,
-  },
   build: {
     outDir: 'dist',
   },
